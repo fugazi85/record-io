c217ff611cd61afcee648a0c8922f2e50fe45f96
DMG <darrengurney232@gmail.com>
2020-06-22 16:06:33 +0100

    screen options dropdown

---
+++ src/render.js
@@ -0,0 +1,28 @@
+// Butts
+const videoElement = document.querySelector('video');
+const startBtn = document.getElementById('startBtn');
+const stopBtn = document.getElementById('stopBtn');
+const videoSelectBtn = document.getElementById('videoSelectBtn');
+const { Menu } = remote;
+videoSelectBtn.onclick = getVideoSources;
+
+
+const { desktopCapturer, remote } = require('electron');
+
+// get video sources
+async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window', 'screen']
+    });
+
+    const videoOptionsMenu = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return {
+                label: source.name,
+                click: () => selectSource(source)
+            };
+        })
+    );
+
+    videoOptionsMenu.popup();
+}
